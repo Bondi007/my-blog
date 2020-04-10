7994127cfe9f9934826dc6e1d196e7f09838902f
Ethel <ethel.maria.benko@gmail.com>
2020-04-10 10:47:31 +0200

    First commit

---
+++ src/build/precache-manifest.a77fab638edad5e2ff56a559732c7cda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1f0cf4380b52e90f3c077916d44716",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7545cbb8a0ced52a1fd5",
+    "url": "/static/css/main.c96dc447.chunk.css"
+  },
+  {
+    "revision": "875d63144a70bf3307d4",
+    "url": "/static/js/2.10b6f616.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.10b6f616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7545cbb8a0ced52a1fd5",
+    "url": "/static/js/main.5daae23b.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
